b49ff5a33bb881f44749ad725802502df58b66e5
voidarclabs <dominicbourne77@gmail.com>
2025-05-04 09:55:01 +0100

    added file manager keybind

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 vim.keymap.set("n", "<C-w>d", "<cmd>lua Snacks.dashboard()<CR>")
 vim.keymap.set("n", "<A-w>d", "<cmd>lua Snacks.dashboard()<CR>")
 vim.keymap.set("n", "<C-p>", "<cmd>JABSOpen<CR>")
+vim.keymap.set("n", "<C-j>", "<cmd>lua MiniFiles.open()<CR>")
 
 vim.keymap.set("n", "<M-w>", "<C-w>")
 vim.keymap.set("n", "<C-k>", "<cmd>FloatermNew lazygit<CR>")
